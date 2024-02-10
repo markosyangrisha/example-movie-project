9efc5c4723673f84c6d82017641b56d747f3702d
markosyangrisha <markosyangrisha70@gmail.com>
2024-02-10 23:58:52 +0400

    added dropdown genre sheet, added hook Debounce, changed the home page structure, added footer

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -6,7 +6,7 @@ export const useDebounce = (value: string, delay?: number) => {
 	useEffect(() => {
 		const time = setTimeout(() => {
 			setDebounce(value)
-		}, delay || 500)
+		}, delay || 400)
 
 		return () => clearTimeout(time)
 	}, [value, delay])
